05d341b22586632e075d57bb6d78a81dd38b5257
Moshiur Rahman <rmoshiur705@gmail.com>
2020-01-10 00:10:49 +0600

    added Delete and Task completed feature

--- js/app.js
+++ js/app.js
@@ -4,28 +4,41 @@ $(document).ready(function() {
       addtask();
     }
   });
+
+  $(document).on("click", ".delete-todo", function() {
+    var id = $(this)
+      .attr("id")
+      .replace("remove_", "");
+    // alert("Delete transaction #" + transaction_id);
+    $("#item_" + id).remove();
+  });
+
+  $(document).on("click", ".main-task", function() {
+    var id = $(this)
+      .attr("id")
+      .replace("main_task_", "");
+    // alert("Delete transaction #" + transaction_id);
+    $("#main_task_" + id).addClass("completed");
+  });
 });
 
 function addtask() {
   let task = $("#task").val();
   if (task !== "") {
+    count = $("#task-list li").length + 1;
+
     var li =
-      "<li class='task'><span class='delete-todo'>X</span><span class='main-task'>" +
+      "<li class='task' id='item_" +
+      count +
+      "'><span class='delete-todo' id='remove_" +
+      count +
+      "'>X</span><span class='main-task' id='main_task_" +
+      count +
+      "'>" +
       task +
       "</span></li>";
+
     $("#task-list").append(li);
     $("#task").val("");
   }
 }
-
-$("li").on("click", function() {
-  $(this).toggoleClass("completed");
-}); //end li click event
-
-$("span").on("click", function(event) {
-  $(this)
-    .parent()
-    .fadeOut()
-    .remove();
-  event.stopPropagation();
-});
