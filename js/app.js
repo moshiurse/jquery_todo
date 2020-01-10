c7d491cc62ae056a10168db2a121e0b742ef2491
Moshiur Rahman <rmoshiur705@gmail.com>
2020-01-10 16:59:38 +0600

    Connected with LocalStorage

--- js/app.js
+++ js/app.js
@@ -1,16 +1,38 @@
 $(document).ready(function() {
+  var id = 0;
+  var taskList = [];
+  // localStorage.clear();
+  if (!localStorage.getItem("total")) {
+    localStorage.setItem("total", id);
+  } else {
+    id = localStorage.getItem("total");
+  }
+
+  if (!localStorage.getItem("taskList")) {
+    localStorage.setItem("taskList", taskList);
+  } else {
+    taskList = JSON.parse(localStorage.getItem("taskList"));
+  }
+
   $("#task").on("keypress", function(e) {
     if (e.keyCode === 13 || e.which === 13) {
       addtask();
     }
   });
 
+  generatedList();
+
   $(document).on("click", ".delete-todo", function() {
     var id = $(this)
       .attr("id")
       .replace("remove_", "");
-    // alert("Delete transaction #" + transaction_id);
+
     $("#item_" + id).remove();
+    var newTask = taskList.filter(function(task) {
+      return task.id != id;
+    });
+    addItemToLocalStorage(newTask);
+    console.log(newTask);
   });
 
   $(document).on("click", ".main-task", function() {
@@ -18,27 +40,77 @@ $(document).ready(function() {
       .attr("id")
       .replace("main_task_", "");
     // alert("Delete transaction #" + transaction_id);
-    $("#main_task_" + id).addClass("completed");
+    $("#main_task_" + id).toggleClass("completed");
+
+    var newData = taskList.map(el => {
+      if (el.id == id) {
+        console.log(el);
+        if (el.completed == false) {
+          // alert("haha");
+          return Object.assign({}, el, { completed: true });
+        } else {
+          // alert("false");
+          return Object.assign({}, el, { completed: false });
+        }
+      }
+      return el;
+    });
+
+    addItemToLocalStorage(newData);
+    console.log(taskList);
   });
-});
 
-function addtask() {
-  let task = $("#task").val();
-  if (task !== "") {
-    count = $("#task-list li").length + 1;
+  function addtask() {
+    let task = $("#task").val();
+    if (task !== "") {
+      id++;
+      // localStorage.setItem("total", id);
+      var item = {
+        id: id,
+        title: task,
+        completed: false,
+        date: new Date()
+      };
 
-    var li =
-      "<li class='task' id='item_" +
-      count +
-      "'><span class='delete-todo' id='remove_" +
-      count +
-      "'>X</span><span class='main-task' id='main_task_" +
-      count +
-      "'>" +
-      task +
-      "</span></li>";
+      // alert(localStorage.getItem("total"));
 
-    $("#task-list").append(li);
-    $("#task").val("");
+      taskList.push(item);
+      addItemToLocalStorage(taskList);
+      var li =
+        "<li class='task' id='item_" +
+        id +
+        "'><span class='delete-todo' id='remove_" +
+        id +
+        "'>X</span><span class='main-task' id='main_task_" +
+        id +
+        "'>" +
+        task +
+        "</span></li>";
+
+      $("#task-list").append(li);
+      $("#task").val("");
+    }
   }
-}
+
+  function addItemToLocalStorage(taskList) {
+    localStorage.removeItem("taskList");
+    localStorage.setItem("taskList", JSON.stringify(taskList));
+  }
+
+  function generatedList() {
+    var lists = JSON.parse(localStorage.getItem("taskList"));
+    lists.map(list => {
+      var li =
+        "<li class='task' id='item_" +
+        list.id +
+        "'><span class='delete-todo' id='remove_" +
+        list.id +
+        "'>X</span><span class='main-task' id='main_task_" +
+        list.id +
+        "'>" +
+        list.title +
+        "</span></li>";
+      $("#task-list").append(li);
+    });
+  }
+});
